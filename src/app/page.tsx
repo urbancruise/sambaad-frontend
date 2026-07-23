b916ab4e82b8ac9ed7cf6dbe467ad93ac3a64b63
urbancruiseit <uchoit01@gmail.com>
2026-07-23 18:08:30 +0530

    fixed bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/src/features/auth/components/auth/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className="fixed inset-0 flex items-center justify-center bg-slate-100">
+    // <main className="fixed inset-0 flex items-center justify-center bg-slate-100">
       <LoginForm />
-    </main>
+    // </main>
   );
 }
