5d35409a4fd8d405d1e3013e1a418e7fb09b3bc1
urbancruiseit <uchoit01@gmail.com>
2026-07-23 18:06:06 +0530

    ts congif fix

--- src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/src/features/auth/components/auth/LoginForm";
-
-export default function LoginPage() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-slate-100">
-      <LoginForm />
-    </div>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import Login from '@/src/app/auth/login/page'
-export default function Home() {
-    return <Login />;
+import LoginForm from "@/src/features/auth/components/auth/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main className="fixed inset-0 flex items-center justify-center bg-slate-100">
+      <LoginForm />
+    </main>
+  );
 }
